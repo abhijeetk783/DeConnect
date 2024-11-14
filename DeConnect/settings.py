7e1bb1efd627f07e909506776af3affddc536558
abhijeetk783 <abhijeetk783@gmail.com>
2024-11-14 12:22:37 -0600

    updated db settings

--- DeConnect/settings.py
+++ DeConnect/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'DeConnect.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
 
 
