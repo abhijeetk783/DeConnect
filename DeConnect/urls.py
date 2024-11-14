fd4e627daa342bef65bba94f920b3dd38bad25d8
abhijeetk783 <abhijeetk783@gmail.com>
2024-11-14 13:21:02 -0600

    media urls added

--- DeConnect/urls.py
+++ DeConnect/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,10 @@ urlpatterns = [
     path('feed/', include(("feed.urls", "feed"), namespace = "feed")),
     path('', include(("signup.urls", "signup"), namespace = "signup")),
 ]
+
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+
+if settings.DEBUG:
+    urlpatterns.extend(
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
